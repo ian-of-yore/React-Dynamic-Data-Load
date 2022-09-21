bca99e58d68f95f7c52ad1cf7931cf78aad569b1
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-09-21 22:53:27 +0600

    React Dynamic Data Load short project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+
+  return (
+    <div className="user-container">
+      <h1>External Users: {users.length}</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: "2px solid maroon", borderRadius: "10px", margin: "10px", backgroundColor: "white" }}>
+      <h3>Name: {props.name}</h3>
+      <h5>Email: {props.email} </h5>
+    </div>
+  )
+}
+
 
 export default App;
